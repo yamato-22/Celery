a011ee0ad2d15885155e8a6faedcfc66980b5a88
Yamato <yamato.altay@gmail.com>
2026-01-04 00:41:06 +0700

    change example

--- request_example.py
+++ request_example.py
@@ -9,7 +9,7 @@ with open("app/files/lama_300px.png", 'rb') as image:
     task_id = response.json()['task_id']
 
 # Ждем завершения обработки нашего файла
-status = "WAITING"
+status = "WAIT..."
 while status not in {"SUCCESS", "FAILURE"}:
     time.sleep(1.0)
     response = requests.get(f"http://127.0.0.1:5000/task/{task_id}").json()
@@ -24,5 +24,5 @@ print(f'{result=}')
 response = requests.get(f"http://127.0.0.1:5000/processed/{result}")
 print(response.status_code)
 if response.status_code == 200:
-    with open("app/files/new_lama_600px.png", 'wb') as f:
+    with open("app/files/lama_600px.png", 'wb') as f:
         f.write(response.content)
