beba8a7edc1eec88c51ca72637aa261c1c8f26ff
Yamato <yamato.altay@gmail.com>
2026-01-04 23:38:12 +0700

    change upscale (one load model)

--- app/upscale.py
+++ app/upscale.py
@@ -1,18 +1,19 @@
 import cv2
 from cv2 import dnn_superres
 
+# Загружаем модель и скейлер при старте
+scaler = dnn_superres.DnnSuperResImpl_create()
+scaler.readModel('models/EDSR_x2.pb')
+scaler.setModel("edsr", 2)
 
-def upscale(input_path: str, output_path: str, model_path: str = 'models/EDSR_x2.pb') -> None:
+def upscale(input_path: str, output_path: str) -> None:
     """
-    :param input_path: путь к изображению для апскейла
-    :param output_path:  путь к выходному файлу
-    :param model_path: путь к ИИ модели
+    Апскейлинг изображения.
+    :param input_path: путь к входному изображению
+    :param output_path: путь к выходному изображению
     :return:
     """
 
-    scaler = dnn_superres.DnnSuperResImpl_create()
-    scaler.readModel(model_path)
-    scaler.setModel("edsr", 2)
     image = cv2.imread(input_path)
     result = scaler.upsample(image)
     cv2.imwrite(output_path, result)
