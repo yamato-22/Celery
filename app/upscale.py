ddf4dbda19d3c720222128aa06b655f1cc6f1968
Yamato <yamato.altay@gmail.com>
2026-01-05 18:56:27 +0700

    change upscale - init model, change config.py - add MODEL_PATH

--- app/upscale.py
+++ app/upscale.py
@@ -1,18 +1,32 @@
 import cv2
 from cv2 import dnn_superres
+from app.config import MODEL_PATH
 
-# Загружаем модель и скейлер при старте
-scaler = dnn_superres.DnnSuperResImpl_create()
-scaler.readModel('models/EDSR_x2.pb')
-scaler.setModel("edsr", 2)
+# Глобальная переменная для хранения экземпляра модели
+scaler = None
+
+
+def initialize_scaler(model_path = MODEL_PATH):
+    """
+    # Загружаем модель и скейлер
+    """
+
+    global scaler
+    if scaler is None:
+        scaler = dnn_superres.DnnSuperResImpl_create()
+        scaler.readModel(model_path)
+        scaler.setModel("edsr", 2)
+
 
 def upscale(input_path: str, output_path: str) -> None:
     """
-    Апскейлинг изображения.
-    :param input_path: путь к входному изображению
-    :param output_path: путь к выходному изображению
+    :param input_path: путь к изображению для апскейла
+    :param output_path: путь к выходному файлу
     :return:
     """
+    # Проверяем, была ли модель уже инициализирована
+    if scaler is None:
+        initialize_scaler()
 
     image = cv2.imread(input_path)
     result = scaler.upsample(image)
